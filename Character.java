9ce3a5df5b9dfc7972c31e11d37793f95ceff3fe
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-07 13:59:17 -0700

    Update character file

--- Character.java
+++ Character.java
@@ -15,8 +15,24 @@ public interface Character {
 
     int maxDmg = 0;
 
-    int maxCooldown = 0;
+    int specialCooldown = 0;
 
+    /* Will be worked on later */
+    public default int basicAtk() {
+        return hitPoints;
+    }
 
+    /* Will be worked on later */
+    public default int specialAtk() {
+        return atkSpd;
+    }
+    public default int getHitPoints() {
+        return hitPoints;
+    }
+
+    /* Will be worked on later */
+    public default void setHitPoints(int hp) {
+
+    }
 
 }
