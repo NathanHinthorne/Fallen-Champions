7310b106baf41b9e40f9fd0e16b5bdf420b0e25d
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-07 12:54:38 -0700

    Progress on character class and update xml file

--- Character.java
+++ Character.java
@@ -1,2 +1,16 @@
-public class Character {
+
+import java.util.*;
+
+public interface Character {
+
+    final static Random MY_RANDOM = new Random();
+
+    public int hitPoints = 0;
+
+    public int atkSpd = 0;
+
+    public float hitChance = 0;
+
+
+
 }
