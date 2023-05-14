900d683c5a6a1e3a2dc1eef538eb2274503b51f5
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-14 14:52:35 -0700

    Add Robot specials and set them up

--- model/Robot.java
+++ model/Robot.java
@@ -12,8 +12,48 @@ public class Robot extends Hero {
 
 
     protected Robot(int theHitPoints, int theAtkSpd, float theHitChance, int theMinDmg, int theMaxDmg, int theCooldown) {
-        super(theHitPoints, theAtkSpd, theHitChance, theMinDmg, theMaxDmg, theCooldown);
+        super(500, 2, 35, 170, 230, 25);
+        setSpecial2Cooldown(35);
+        setSpecial3Cooldown(40);
+        setSpecial4Cooldown(55);
+        setSpecial5Cooldown(100);
     }
 
+    public void setSpecial2Cooldown(int thespecial2Cooldown) {
+        thespecial2Cooldown = special2Cooldown;
+    }
+
+    public void setSpecial3Cooldown(int thespecial3Cooldown) {
+        thespecial3Cooldown = special3Cooldown;
+    }
+
+    public void setSpecial4Cooldown(int thespecial4Cooldown) {
+        thespecial4Cooldown = special4Cooldown;
+    }
+
+    public void setSpecial5Cooldown(int thespecial5Cooldown) {
+        thespecial5Cooldown = special5Cooldown;
+    }
+
+
+    /* Will be worked on later */
+    public void specialAtk2() {
+
+    }
+
+    /* Will be worked on later */
+    public void specialAtk3() {
+
+    }
+
+    /* Will be worked on later */
+    public void specialAtk4() {
+
+    }
+
+    /* Will be worked on later */
+    public void specialAtk5() {
+
+    }
 
 }
