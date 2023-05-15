79426252e635ee1c0f68bc9241e0260f332c33e3
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-14 19:04:01 -0700

    Add Boss class 😎

--- model/Boss.java
+++ model/Boss.java
@@ -4,7 +4,7 @@ public class Boss extends Monster {
 
 
     protected Boss(int theHitPoints, int theAtkSpd, float theHitChance, int theMinDmg, int theMaxDmg, int theCooldown, int theMinHeal, int theMaxHeal, float theHealChance) {
-        super(1500, 1, 98, 90, 230, 40, 130, 290, 99);
+        super(1500, 1, 97, 90, 230, 40, 130, 290, 99);
     }
 
     public void specialAtk2() {
