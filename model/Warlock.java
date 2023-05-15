fbc883447d7e94ad165a1aa8bbfe5b5041b2e807
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-14 19:49:15 -0700

    Add special attack for Warlock

--- model/Warlock.java
+++ model/Warlock.java
@@ -8,6 +8,21 @@ public class Warlock extends Monster {
     }
 
     public void specialAtk2() {
+        if(getHitPoints() < 0)
+        {
+            throw new IllegalArgumentException("Hit Points cannot be less than zero");
+        }
+
+        getHitChance();
+
+        if(getHitChance() > 75) {
+            // Attack successful
+            setHitPoints(getHitPoints() - getMaxDamage());
+        } else { // Will be worked on later, planned to be an error message
+            // Attack failed
+        }
+
+        //getSpecialCooldown() = MAX_SPECIAL_COOLDOWN; // reset the cooldown
 
     }
 }
