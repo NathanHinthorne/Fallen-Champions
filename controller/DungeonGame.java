0707b0e2782bee8d86c4e039ad559d1d84328a86
Brendan <49573929+DaddyDoplr@users.noreply.github.com>
2023-05-18 12:03:45 -0700

    Added a MonsterFactory.java interface, and a quick (nonfunctional) instantiation of a Hero type in DungeonGame.java

--- controller/DungeonGame.java
+++ controller/DungeonGame.java
@@ -1,4 +1,16 @@
 package controller;
+import model.*;
 
 public class DungeonGame {
+
+    private Hero myHero;
+
+    private DunegeonGame() {
+        myHero = new Warrior();
+    }
+
+    public static void main(String[] theArgs) {
+
+    }
+
 }
