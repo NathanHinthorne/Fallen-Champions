8d3d4c8abc823630393f736502c5b58dc6dfa836
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-20 19:43:34 -0700

    Attempt to add SQLite into controller

--- controller/DungeonGame.java
+++ controller/DungeonGame.java
@@ -1,4 +1,11 @@
 package controller;
+
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import model.*;
 
 public class DungeonGame {
@@ -10,13 +17,26 @@ public class DungeonGame {
     public Monster myMonster;
 
     private DungeonGame() {
+        SQLiteDataSource ds = null;
+
+        try {
+            ds = new SQLiteDataSource();
+            ds.setUrl("jdbc:sqlite:monsters.db");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
 
         /* These Hero and Monster factory uses are temporary,
          * to see if they work properly.
          */
         myHero = HERO_FACTORY.buildHero("");
         myMonster = MONSTER_FACTORY.buildMonster("");
+
+
     }
+
+
 
     public static void main(String[] theArgs) {
         System.out.println("GAME CREATED");
