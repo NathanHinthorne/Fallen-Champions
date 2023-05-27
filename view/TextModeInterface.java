b74a2094e7d5a2dd4fe17cbb7ee990e0d7661621
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-26 21:05:22 -0700

    Continue work on Text Mode Interface

--- view/TextModeInterface.java
+++ view/TextModeInterface.java
@@ -1,6 +1,23 @@
 package view;
+import model.*;
+import controller.DungeonGame;
 
 public class TextModeInterface {
+
+    public void menu() {
+
+    }
+
+    public boolean is_ongoing(int ongoing) {
+        if(ongoing == 1) {
+            return true;
+        } else if(ongoing == 0) {
+            return false;
+        }
+
+        return false;
+    }
+
 
 
 }
--- view/splashScreen.java
+++ view/splashScreen.java
@@ -1,6 +1,5 @@
 package view;
-import model.*;
-import controller.DungeonGame;
+
 
 
 public class splashScreen {
