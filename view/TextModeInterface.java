5a498b81ca74c8a722753b0a07edc561f66c7ac3
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-05-27 17:13:59 -0700

    Add logic inside Menu() method

--- view/TextModeInterface.java
+++ view/TextModeInterface.java
@@ -1,10 +1,28 @@
 package view;
 import model.*;
 import controller.DungeonGame;
+import java.util.Scanner;
 
 public class TextModeInterface {
 
     public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.println("Welcome to Fallen Champions, TUI edition");
+            System.out.println("Now make a selection!");
+            System.out.println("1 to start game, 2 to exit game");
+            System.out.println("Now make your selection: ");
+            int selection = sc.nextInt();
+            switch(selection) {
+                case 1:
+                    // Start Game
+                case 2:
+                    System.exit(0);
+                default:
+                    System.out.println("Please make a proper selection:");
+            }
+        }
+
 
     }
 
