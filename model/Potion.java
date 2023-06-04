4511b0901d7be0964847f437716ce00113f20ab8
Brendan <49573929+DaddyDoplr@users.noreply.github.com>
2023-06-04 16:04:12 -0700

    Fixed Health Potions

--- model/Potion.java
+++ model/Potion.java
@@ -6,6 +6,11 @@ public class Potion {
     public String inventoryTextDisplay() {
         return "Debug Potion";
     }
+
+    public int getDetail(Hero thePlayer) {
+        return 0;
+    }
+
     public int effect(Hero thePlayer) {
         // For a health potion, it will return the healing amount.
         // For a vision potion, it will return the visible radius.
