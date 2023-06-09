01b8a1b7d0cb331827c639e55138bdf7553fa7b4
Brendan <49573929+DaddyDoplr@users.noreply.github.com>
2023-06-09 03:01:39 -0700

    Hero and Monster factory tests added

--- model/unittest/HeroFactoryTest.java
+++ model/unittest/HeroFactoryTest.java
@@ -1,14 +1,47 @@
 package model.unittest;
 
+import model.HeroFactory;
+import model.Hero;
+import model.HeroTypes;
 import org.junit.Test;
-
-import static org.junit.*;
+import static org.junit.Assert.*;
 
 class HeroFactoryTest {
 
-    @Test
-    void assertEquals() {
+    private static HeroFactory factory = new HeroFactory();
 
+    @Test
+    public void testBuildHeroEnforcer() {
+        HeroTypes e = HeroTypes.ENFORCER;
+        Hero hero = factory.buildHero(e);
+        assertEquals(hero.getType(), e);
     }
 
+    @Test
+    public void testBuildHeroWarrior() {
+        HeroTypes e = HeroTypes.WARRIOR;
+        Hero hero = factory.buildHero(e);
+        assertEquals(hero.getType(), e);
+    }
+
+    @Test
+    public void testBuildHeroSupport() {
+        HeroTypes e = HeroTypes.SUPPORT;
+        Hero hero = factory.buildHero(e);
+        assertEquals(hero.getType(), e);
+    }
+
+    @Test
+    public void testBuildHeroScientist() {
+        HeroTypes e = HeroTypes.SCIENTIST;
+        Hero hero = factory.buildHero(e);
+        assertEquals(hero.getType(), e);
+    }
+
+    @Test
+    public void testBuildHeroRobot() {
+        HeroTypes e = HeroTypes.ROBOT;
+        Hero hero = factory.buildHero(e);
+        assertEquals(hero.getType(), e);
+    }
 }
---
+++ model/unittest/MonsterFactoryTest.java
@@ -0,0 +1,44 @@
+package model.unittest;
+
+import model.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class MonsterFactoryTest {
+    private static MonsterFactory factory = new MonsterFactory();
+
+    @Test
+    public void testBuildMonsterEnforcer() {
+        MonsterTypes e = MonsterTypes.SKELETON;
+        Monster monster = factory.buildMonster(e);
+        assertEquals(monster.getType(), e);
+    }
+
+    @Test
+    public void testBuildMonsterWarrior() {
+        MonsterTypes e = MonsterTypes.OGRE;
+        Monster monster = factory.buildMonster(e);
+        assertEquals(monster.getType(), e);
+    }
+
+    @Test
+    public void testBuildMonsterSupport() {
+        MonsterTypes e = MonsterTypes.BOSS;
+        Monster monster = factory.buildMonster(e);
+        assertEquals(monster.getType(), e);
+    }
+
+    @Test
+    public void testBuildMonsterScientist() {
+        MonsterTypes e = MonsterTypes.GREMLIN;
+        Monster monster = factory.buildMonster(e);
+        assertEquals(monster.getType(), e);
+    }
+
+    @Test
+    public void testBuildMonsterRobot() {
+        MonsterTypes e = MonsterTypes.WARLOCK;
+        Monster monster = factory.buildMonster(e);
+        assertEquals(monster.getType(), e);
+    }
+}
