2418ac8690929402c89e4fab9720c2028df75e8c
Nathan Hinthorne <nathanrh@uw.edu>
2023-06-09 20:45:01 -0700

    Changes by Austin

--- view/TUI.java
+++ view/TUI.java
@@ -5,10 +5,22 @@ import model.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * TUI mode of the game
+ *
+ * @author Austin Roaf
+ * @author Brendan Smith
+ * @author Nathan Hinthorne
+ * @version 1.0
+ */
 public class TUI {
 
     private static final Scanner THE_SCANNER = new Scanner(System.in);
 
+    /**
+     * Main menu of the game
+     * @return main menu input
+     */
     public int menu() {
         System.out.println("Welcome to Fallen Champions, TUI edition");
         System.out.println("1 to start game, 2 to exit game");
@@ -16,18 +28,29 @@ public class TUI {
         return THE_SCANNER.nextInt();
     }
 
+    /**
+     * Chooses the hero
+     * @return the hero choice
+     */
     public int chooseHero() {
         System.out.println("Choose your hero!");
         System.out.println("1 for Enforcer, 2 for Robot, 3 for Support, 4 for Scientist, 5 for Warrior");
         return THE_SCANNER.nextInt();
     }
 
+    /**
+     * Introduces you to the game
+     */
     public void Introduction() {
         System.out.println("It's a bright, sunny day out. You enjoy the weather, have fun, do whatever, until one day...");
         System.out.println("You suddenly wake up in a deep dark dungeon with no memory, stuck in an ancient dungeon");
         System.out.println("You then realize that you are yourself a Fallen Champion! Can you escape the dungeon?");
     }
 
+    /**
+     * Gameplay menu
+     * @return gameplay menu choice
+     */
     public int gameplayMenu() {
         System.out.println();
         System.out.println("-----------------------------------------------------");
@@ -39,6 +62,12 @@ public class TUI {
         return THE_SCANNER.next().charAt(0);
     }
 
+    /**
+     * Battle menu for battling monsters
+     * @param theHero you
+     * @param theMonster the monster you're battling
+     * @return battle menu input
+     */
     public int battleMenu(Hero theHero, Monster theMonster) { // is this parameter needed?
         double playerPerc = (theHero.getHitPoints() / theHero.getMaxHitPoints()) * 20.0;
         System.out.println("-------------------");
@@ -61,11 +90,21 @@ public class TUI {
         return intret;
     }
 
+    /**
+     * Menu for choosing the difficulty
+     * @return the difficulty choice
+     */
     public int chooseDifficulty() {
         System.out.println("Choose your difficulty!");
         System.out.println("1 for easy, 2 for medium, 3 for hard");
         return THE_SCANNER.nextInt();
     }
+
+    /**
+     * Menu for inventory choice
+     * @param myBag your inventory bag
+     * @return the inventory choice
+     */
     public int openBag(final Inventory myBag) {
         System.out.println("Opening bag...");
         DelayMachine.delay(3);
@@ -90,6 +129,11 @@ public class TUI {
         return index;
     }
 
+    /**
+     * Potion selection
+     * @param theVal the potion you selected
+     * @param thePlayer you
+     */
     public void usePotion(int theVal, Hero thePlayer) {
         Potion potion = thePlayer.getMyInventory().getPotionInventory().get(theVal);
 
@@ -101,6 +145,10 @@ public class TUI {
         }
     }
 
+    /**
+     * Brings you a menu for whether you want to quit the game or not
+     * @return the menu selection
+     */
     public int quitProcess() {
         System.out.println("Are you sure you want to quit?");
         System.out.println("1 for yes, 2 for no");
@@ -108,11 +156,19 @@ public class TUI {
 
     }
 
+    /**
+     * Displays the dungeon map
+     * @param theDungeon the map
+     */
     public void displayDungeonMap(Dungeon theDungeon) {
         System.out.println(theDungeon.toString());
         System.out.println();
     }
 
+    /**
+     * Displays the hero info
+     * @param theHero you
+     */
     public void displayHeroInfo(final Hero theHero) {
 
         System.out.println("Stats");
@@ -126,38 +182,63 @@ public class TUI {
 
     }
 
+    /**
+     * Displays the potion info
+     * @param thePotion the potion info
+     */
     public void displayPotionInfo(final Potion thePotion) {
         System.out.println("You collected a " + thePotion.type() + "!");
     }
+
+    /**
+     * Displays Abstraction Pillar Message
+     */
     public void displayAbstractionPillarMsg() {
         System.out.println("You collected an Abstraction Pillar!");
     }
-
+    /**
+     * Displays Encapsulation Pillar Message
+     */
     public void displayEncapsulationPillarMsg() {
         System.out.println("You collected an Encapsulation Pillar!");
     }
-
+    /**
+     * Displays Inheritance Pillar Message
+     */
     public void displayInheritancePillarMsg() {
         System.out.println("You collected an Inheritance Pillar!");
     }
-
+    /**
+     * Displays Polymorphism Pillar Message
+     */
     public void displayPolymorphismPillarMsg() {
         System.out.println("You collected a Polymorphism Pillar!");
     }
 
 
-
+    /**
+     * Asks whether you want to start a new game or continue on a prior save
+     * @return the game choice
+     */
     public int continueOrNewGameMenu() {
         System.out.println("Would you like to start a new game or continue a previous game?");
         System.out.println("1 for new game, 2 for continue game");
         return THE_SCANNER.nextInt();
     }
 
+    /**
+     * The monster turn text
+     */
     public void monsterTurnText() {
         System.out.println("-------------------");
         System.out.println("The Enemy is making their move...");
     }
 
+    /**
+     * The monster move text
+     * @param theVal the value
+     * @param theHpAmt the monster hp amt
+     */
     public void monsterMoves(int theVal, int theHpAmt) {
         if (theVal == 0) {
             if (theHpAmt == 0) {
@@ -177,6 +258,12 @@ public class TUI {
         }
     }
 
+    /**
+     * The player move
+     * @param theVal the player choice value
+     * @param theHpAmt the hp amt
+     * @param thePlayer you
+     */
     public void playerMoves(int theVal, int theHpAmt, Hero thePlayer) {
         System.out.println("-------------------");
         if (theVal == 1) {
@@ -201,38 +288,64 @@ public class TUI {
         }
     }
 
+    /**
+     * The game victory message
+     */
     public void displayVictoryMsg() {
         System.out.println("You have escaped the dungeon!. Congratulations!!");
     }
 
+    /**
+     * The battle win message
+     */
     public void displayBattleWinMsg() {
         System.out.print("ENEMY DEFEATED! GET REKT!!");
         System.out.println();
         System.out.println("-----------------------------------------------------");
     }
 
+    /**
+     * The death message
+     */
     public void displayDeathMsg() {
         System.out.println("You have been defeated. GIT GUD SCRUB!");
     }
 
+    /**
+     * The cheat mode message
+     */
     public void displayCheatModeMsg() {
         System.out.println("*****************************************************");
         System.out.println("YOU CHEATER! I SEE WHAT YOU'RE DOING THERE! ಠ_ಠ");
         System.out.println("*****************************************************");
     }
 
+    /**
+     * The hero health
+     * @param theHero you
+     */
     public void displayHeroHealth(final Hero theHero) {
         System.out.println("Health: " + theHero.getHitPoints() + "/" + theHero.getMaxHitPoints());
     }
 
+    /**
+     * The game start message
+     */
     public void displayStartMsg() {
         System.out.println("Welcome to the Dungeon!");
     }
 
+    /**
+     * The instant kill message
+     */
     public void displayInstaKill() {
         System.out.println("Woooow, you must be so powerful... cheater");
     }
 
+    /**
+     * Monster encounter message
+     * @param theMonster the monster you're encountering
+     */
     public void displayMonsterEncounterMsg(final Monster theMonster) {
         System.out.println("You have encountered a " + theMonster.getType().toString());
     }
