3bc3266da5957c719783893a0fce9c7fa943ea9d
GenSayer <64618080+GenSayer@users.noreply.github.com>
2023-06-09 17:31:05 -0700

    Continue Test Room

--- Test/TestRoom.java
+++ Test/TestRoom.java
@@ -4,14 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class TestRoom {
 
     Room room;
 
     List<Monster> monsterList = new LinkedList<>();
+
+    Set<Pillars> thePillars = new HashSet<>();
 
     @Test
     public void testMonsterPlacement() {
@@ -21,6 +25,56 @@ public class TestRoom {
         monsterList.add(monsterFactory.buildMonster(MonsterTypes.SKELETON));
 
         room.placeMonster(monsterList);
+        assertTrue(room.hasMonster());
+    }
+
+    @Test
+    public void testPitPlacement() {
+        room = new Room(0,0);
+
+        room.placePit();
+        assertTrue(room.hasPit());
+    }
+
+    @Test
+    public void testHeroPlacement() {
+        room = new Room(0,0);
+
+        room.placeHero();
+        assertTrue(room.hasHero());
+    }
+
+    @Test
+    public void testPotionPlacement() {
+        room = new Room(0,0);
+
+        room.placePotion();
+        assertTrue(room.hasPotion());
+    }
+
+    @Test
+    public void testExitPlacement() {
+        room = new Room(0,0);
+
+        room.placeExit();
+        assertTrue(room.hasExit());
+    }
+
+    @Test
+    public void testWallPlacement() {
+        room = new Room(0,0);
+
+        room.placeWall();
+        assertTrue(room.hasWall());
+    }
+
+    @Test
+    public void testPillarPlacement() {
+        room = new Room(0,0);
+
+
+
+        room.placePillar(thePillars);
         assertTrue(room.hasMonster());
     }
 }
